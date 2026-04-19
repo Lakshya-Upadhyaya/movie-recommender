4f51f5c7b58ca27df0618fb2a8e3258920d4c030
Lakshya-Upadhyaya <lakshya.upadhyaya05@gmail.com>
2026-04-19 20:45:41 +0530

    Removed redundant frontend

--- frontend/components/chat/index.ts
+++
@@ -1,5 +0,0 @@
-export { ChatInterface } from "./chat-interface";
-export { ChatMessage } from "./chat-message";
-export { ChatInput } from "./chat-input";
-export { TypingIndicator } from "./typing-indicator";
-export { OfflineFallback } from "./offline-fallback";
